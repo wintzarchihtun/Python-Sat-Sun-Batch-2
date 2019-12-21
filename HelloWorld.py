32f65eeb90a11a9a65b483c99c5cd727d6c52aa7
brainseeddeveloper <brainseeddeveloper@gmail.com>
2019-12-21 20:06:45 +0630

    Variable

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,8 @@
 print("Hello World")
 
 1 2 3 4 5
+
+#this is the first commet
+
+spame = 1 # and this is the second comment
+text = "Text #This is not a comment " 
---
+++ Variable.py
@@ -0,0 +1,76 @@
+#21.12.2019
+
+>>> 2 + 2
+4
+>>> 50 - 5 * 6
+20
+>>> (50 - 5 * 6) / 4
+5.0
+>>> round(5.0)
+5
+>>> 5.123456
+5.123456
+>>> round(5.123456,2)
+5.12
+>>> 17 / 3
+5.666666666666667
+>>> 17 // 3
+5
+>>> 18 // 4
+4
+>>> 20 // 5
+4
+>>> 17 % 3
+2
+>>> 17 * 4 % 3
+2
+
+a = 1
+a (variable) = (assign) 1 (value)
+
+width = 20
+height = 5 * 9
+vol = width * height
+vol
+
+sale = 30000
+tax = 5 / 100
+total_tax = sale * tax
+total_tax 
+total_price = sale + total_tax
+total_price
+
+print('spam eggs') 
+print(' don\'t ')
+print("doesn't")
+print('"Yes"')
+print("\"Yes,\" they said.")
+print('"Isn\'t," they said')
+
+# \n  =newLine
+
+s = 'First Line.\nSecond LIne.'
+print(s)
+
+print("""\
+Usage: thingy
+		-a
+		-b
+		-c
+""")
+
+"""...""" or '''...'''
+
+3 * 'A'
+2 * 'BC' + 'DE'
+10 * 'GH' + 3
+
+word = 'Programming'
+word[0]
+word[-2]
+word[3:5]
+word[4:9]
+word[:5]
+word[8:]
+word[5:-3]
+word[:2] + word[3:]
